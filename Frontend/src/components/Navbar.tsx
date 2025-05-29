5e20afdcaf0e6005b0f0dd5e463a000d908e338f
Iram Saba Khan <161953870+iramsk02@users.noreply.github.com>
2025-05-29 13:52:37 +0530

    deployment changes2

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => (
   <nav className="flex justify-between items-center px-8 py-6 bg-black text-white">
-    <h1 className="text-2xl font-semibold"><img src="public\tshirt11.jpg" className="h-10" alt="" /></h1>
+    <h1 className="text-2xl font-semibold"><img src="/tshirt11.jpg" className="h-10" alt="" /></h1>
     <div className="space-x-6">
       <Link to="/">Home</Link>
       <Link to="/">About us</Link>
